fbce5af2d2d5bb06709477920afa781fa923c8f5
Cassandra Rousseau <cass201001@gmail.com>
2023-10-10 18:26:15 -0400

    Completed the queries

--- Assignment01/server.js
+++ Assignment01/server.js
@@ -15,12 +15,12 @@ app.use("/client", clientRoute);
 require("dotenv").config();  
 // console.log(process.env) 
 
-const { MongoClient } = require('mongodb');
-const url = process.env.MONGO_DB_URI;
+const { MongoClient, ObjectId } = require('mongodb');
+const uri = process.env.MONGO_DB_URI;
 // Database Name
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(url, {});
+const client = new MongoClient(uri, {});
 
 
 async function run() {
@@ -31,24 +31,19 @@ async function run() {
     // console.log("success");
     
     const db = await client.db("CART451_Final_Project");
-    const videos = await db.collection("youtube_videos");
+    const videos = await db.collection("youtube_videos", {
+      collation: { locale: "fr_CA",  numericOrdering: true,},});
 
-// Counts the number of input in the collection
+    // **1** Count the number of input in the collection
     const estimate = await videos.estimatedDocumentCount();
     console.log(`Estimated number of documents in the videos collection: ${estimate}`);
     
-    // Display all the data in the console
+    // **2** Display all the data in the console
 let results = await videos.find({}).toArray();
 console.log(results);
 
-// Find one video according to the inputed data catregories
-    const options = {
 
-      projection: {'description':1, 'category':1}
-    }
-    let isInGroup = await videos.findOne({'category':{$in:["beauty","vlogs", "travel", "food"]}}, options)
-    console.log(isInGroup);
-
+//**3** Retrieves the data from the history category and only display their title 
 // Retrieves data
     const yt_video = "title";
     // Specify an optional query document to narrow results
@@ -59,15 +54,54 @@ console.log(results);
     // Print the result
     console.log(distinctValues);
 
-    // let unwound = await videos.aggregate([ 
-    //   $text: {
-    //     $search: "pasta",
-    //     $caseSensitive: true
-    //   }]);
-    // console.log(unwound);
-    let findings = await videos.find( { $text: { $search: "coffee shop" } } );
+    
+// **4**Only find videos with subscribers over 100
+const documentsToFind = {description:{$gt:"100"}}
+let resultat = videos.find(documentsToFind);
+let docCount = videos.countDocuments(documentsToFind);
+await resultat.forEach((doc)=>console.log(doc))
+console.log('Found ${await docCount} documents')
 
-    console.log(findings);
+// **5** Find the video with the specific inserted id
+const document = {_id: new ObjectId("65246667d654716acf8a431e")}
+let answer = await videos.findOne(document)
+console.log("Found document")
+console.log(answer)
+
+// **6** Filter the art and music video category by displaying only the videos with less or equal than 400 subscribers on the channel, and only display their title and descriptions
+const pipeline = [
+{$match: {category:"art_music", description: {$lte:"400"}}},
+{$sort:{description:1}},
+{$project:{
+  _id:0,
+  link:0,
+category:0,
+},}
+]
+
+let filteredResults = await videos.aggregate(pipeline)
+
+for await (const doc of filteredResults){
+  console.log(doc);
+  console.log("mission achieved");
+}
+
+// // **7** Find the food category videos, sort in alphabetical order the results, and limit the results to 5 videos
+// const neededDocuments = {category:"food"}
+// let foundResults = await videos.find(neededDocuments).sort({title:1}).limit(5);
+// await foundResults.forEach((doc)=>console.log(doc));
+// console.log('Found ${await docCounting} documents');
+
+
+// //**8**Find one video according to the inputed data categories
+// const options = {
+
+//   projection: {'description':1, 'category':1}
+// }
+// let isInGroup = await videos.findOne({'category':{$in:["beauty","vlogs", "travel", "food"]}}, options)
+// console.log(isInGroup);
+
+
 } // in try 
 catch (error) {
     console.error("error::");
