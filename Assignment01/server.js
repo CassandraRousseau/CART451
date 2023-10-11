a45151c3f991f56de8e6b0a1fb25030d409b807d
Cassandra Rousseau <cass201001@gmail.com>
2023-10-10 20:23:51 -0400

    Trying to make get request

--- Assignment01/server.js
+++ Assignment01/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const portNumber = 4200;
+const portNumber = 8080;
 const app = express(); //make an instance of express
 const server = require("http").createServer(app);
 
@@ -23,101 +23,101 @@ const uri = process.env.MONGO_DB_URI;
 const client = new MongoClient(uri, {});
 
 
-async function run() {
-  try {
+// async function run() {
+//   try {
     
-    await client.connect();
-    await client.db("admin").command({ping:1});
-    // console.log("success");
+//     await client.connect();
+//     await client.db("admin").command({ping:1});
+//     // console.log("success");
     
-    const db = await client.db("CART451_Final_Project");
-    const videos = await db.collection("youtube_videos", {
-      collation: { locale: "fr_CA",  numericOrdering: true,},});
+//     const db = await client.db("CART451_Final_Project");
+//     const videos = await db.collection("youtube_videos", {
+//       collation: { locale: "fr_CA",  numericOrdering: true,},});
 
-    // **1** Count the number of input in the collection
-    const estimate = await videos.estimatedDocumentCount();
-    console.log(`Estimated number of documents in the videos collection: ${estimate}`);
+//     // **1** Count the number of input in the collection
+//     const estimate = await videos.estimatedDocumentCount();
+//     console.log(`Estimated number of documents in the videos collection: ${estimate}`);
     
-    // **2** Display all the data in the console
-let results = await videos.find({}).toArray();
-console.log(results);
+//     // **2** Display all the data in the console
+// let results = await videos.find({}).toArray();
+// console.log(results);
 
 
-//**3** Retrieves the data from the history category and only display their title 
-// Retrieves data
-    const yt_video = "title";
-    // Specify an optional query document to narrow results
-    const content = { category: "history" };
-    // Execute the distinct operation
-    const distinctValues = await videos.distinct(yt_video, content);
+// //**3** Retrieves the data from the history category and only display their title 
+// // Retrieves data
+//     const yt_video = "title";
+//     // Specify an optional query document to narrow results
+//     const content = { category: "history" };
+//     // Execute the distinct operation
+//     const distinctValues = await videos.distinct(yt_video, content);
 
-    // Print the result
-    console.log(distinctValues);
+//     // Print the result
+//     console.log(distinctValues);
 
     
-// **4**Only find videos with subscribers over 100
-const documentsToFind = {description:{$gt:"100"}}
-let resultat = videos.find(documentsToFind);
-let docCount = videos.countDocuments(documentsToFind);
-await resultat.forEach((doc)=>console.log(doc))
-console.log('Found ${await docCount} documents')
+// // **4**Only find videos with subscribers over 100
+// const documentsToFind = {description:{$gt:"100"}}
+// let resultat = videos.find(documentsToFind);
+// let docCount = videos.countDocuments(documentsToFind);
+// await resultat.forEach((doc)=>console.log(doc))
+// console.log('Found ${await docCount} documents')
 
-// **5** Find the video with the specific inserted id
-const document = {_id: new ObjectId("65246667d654716acf8a431e")}
-let answer = await videos.findOne(document)
-console.log("Found document")
-console.log(answer)
+// // **5** Find the video with the specific inserted id
+// const document = {_id: new ObjectId("65246667d654716acf8a431e")}
+// let answer = await videos.findOne(document)
+// console.log("Found document")
+// console.log(answer)
 
-// **6** Filter the art and music video category by displaying only the videos with less or equal than 400 subscribers on the channel, and only display their title and descriptions
-const pipeline = [
-{$match: {category:"art_music", description: {$lte:"400"}}},
-{$sort:{description:1}},
-{$project:{
-  _id:0,
-  link:0,
-category:0,
-},}
-]
+// // **6** Filter the art and music video category by displaying only the videos with less or equal than 400 subscribers on the channel, and only display their title and descriptions
+// const pipeline = [
+// {$match: {category:"art_music", description: {$lte:"400"}}},
+// {$sort:{description:1}},
+// {$project:{
+//   _id:0,
+//   link:0,
+// category:0,
+// },}
+// ]
 
-let filteredResults = await videos.aggregate(pipeline)
+// let filteredResults = await videos.aggregate(pipeline)
 
-for await (const doc of filteredResults){
-  console.log(doc);
-  console.log("mission achieved");
-}
+// for await (const doc of filteredResults){
+//   console.log(doc);
+//   console.log("mission achieved");
+// }
 
-// **7** Find the food category videos, sort in alphabetical order the results, and limit the results to 5 videos
-const neededDocuments = {category:"food"}
-let foundResults = await videos.find(neededDocuments).sort({title:1}).limit(5);
-await foundResults.forEach((doc)=>console.log(doc));
-console.log('Found ${await docCounting} documents');
-
-
-//**8**Find one video according to the inputed data categories
-const options = {
-
-  projection: {'description':1, 'category':1}
-}
-let isInGroup = await videos.findOne({'category':{$in:["beauty","vlogs", "travel", "food"]}}, options)
-console.log(isInGroup);
+// // **7** Find the food category videos, sort in alphabetical order the results, and limit the results to 5 videos
+// const neededDocuments = {category:"food"}
+// let foundResults = await videos.find(neededDocuments).sort({title:1}).limit(5);
+// await foundResults.forEach((doc)=>console.log(doc));
+// console.log('Found ${await docCounting} documents');
 
 
-} // in try 
-catch (error) {
-    console.error("error::");
-    console.log(error);
-    // Expected output: ReferenceError: nonExistentFunction is not defined
-    // (Note: the exact output may be browser-dependent)
-  }
- /* The finally block will always execute before control flow exits the try...catch...finally construct. 
- It always executes, regardless of whether an exception was thrown or caught.*/
-  finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
+// //**8**Find one video according to the inputed data categories
+// const options = {
 
-run()
+//   projection: {'description':1, 'category':1}
+// }
+// let isInGroup = await videos.findOne({'category':{$in:["beauty","vlogs", "travel", "food"]}}, options)
+// console.log(isInGroup);
+
+
+// } // in try 
+// catch (error) {
+//     console.error("error::");
+//     console.log(error);
+//     // Expected output: ReferenceError: nonExistentFunction is not defined
+//     // (Note: the exact output may be browser-dependent)
+//   }
+//  /* The finally block will always execute before control flow exits the try...catch...finally construct. 
+//  It always executes, regardless of whether an exception was thrown or caught.*/
+//   finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
+// run()
 
 
 
@@ -131,10 +131,25 @@ server.listen(portNumber, function () {
 app.get("/", function (req, res) {
   res.send("<h1>Hello world</h1>");
 });
-
+app.get('/', requestHandler);
 function clientRoute(req, res, next) {
   res.sendFile(__dirname + "/public/client.html");
 }
 
+function requestHandler(req,res){
+  // send a default response to the client...
+  response.send("Welcome to CART 451 with Express");
+  console.log(req); //built in
+  console.log(res); //built  in
+  console.log(req.url);
+}
+
+app.post('/aPostEndPoint', postRequestHandler);
+
+function postRequestHandler(req,res){
+    // send a default response to the client...
+    response.send("Post request was made at end point `aPostEndPoint`");
+   console.log(req);
+}
 
 
