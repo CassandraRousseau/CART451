eb55a6f12f7fb4009c7709743f4ba25b5898d9db
Cassandra Rousseau <cass201001@gmail.com>
2023-10-12 17:07:17 -0400

    Completing assignment

--- Assignment01/public/js/client.js
+++ Assignment01/public/js/client.js
@@ -1,88 +1,115 @@
 window.onload = function () {
   console.log("we are loaded");
-  const buttonCallA = document.querySelector("#search_art");
-  // const buttonCallB = document.querySelector('#search_food');
-  // const buttonCallC = document.querySelector("#search_hist");
-  //GET
-  buttonCallA.addEventListener('click', 
-  
-  async function(){
-    const results = document.getElementById("results");
-    let  response = await fetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`);
-    let responseText = await response.text();
-    
+  const buttonCall = document.querySelector("#search");
+  const responseA = document.querySelector("#responseA");
+const responseB = document.querySelector("#responseB");
+const responseC = document.querySelector("#responseC");
+const responseD = document.querySelector("#responseD");
+const responseE = document.querySelector("#responseE");
+//     const results = document.getElementById("results");
+
+//     let responseText = await response.text();
     // results.innerHTML = responseText;
-    // Add a blank space between each line of the response text
+//     // Add a blank space between each line of the response text
 
-console.log("the response::: ", responseText);
+// console.log("the response::: ", responseText);
 
-results.innerHTML = responseText;
+// results.innerHTML = responseText;
 
-  });//click
-
-  function changeString_A(fruitString) {
-    return new Promise((resolve, reject) => {
-      //console.log(userString);
-      setTimeout(() => {
-        console.log("time-out two-a complete " + fruitString);
-        let userFruitwithstars = fruitString.split("").join("*");
-        resolve(userFruitwithstars);
-      }, 5000); // let 5 secs go past then send back
-    });
+//   });//click
+  function getJSONFromServerUsingFetch(fileNameLink) {
+    // use ES6 fetch API, which return a promise
+    return fetch(fileNameLink).
+    then(function(r) 
+    { 
+      //what is returned here is ALSO A PROMISE 
+      return r.text()
+    })
   }
 
-//returns a promise
+  buttonCall.addEventListener("click", function () {
+    console.log("clicked");
+    let returnedFruit = checkAuth_A(
+      document.querySelector("#user-string-A").value
+    );
+    changeString_A(returnedFruit);
+    });
+  
 
 function checkAuth_A(userString) {
-    //A:: this function will return the promise
-    return new Promise((resolve, reject) => {
-      console.log("Checking Auth...");
-      console.log("user string:: " + userString);
-  
-      setTimeout(() => {
-        let userFruit = "";
-        console.log("time-out one-a complete " + userFruit);
-        if (userString === "Sabine") {
-          userFruit = "pineapple";
-          resolve(userFruit);
-        } else {
-          userFruit = "noFruit";
-          reject("error: no fruit");
-        }
-      }, 5000);
-    });
+  setTimeout(() => {
+    let answer = "";
+    if (userString === "PayPal") {
+      getJSONFromServerUsingFetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`)
+      .then(
+        function (resultFromFetchA) {
+          console.log(resultFromFetchA);
+          let sRes =  resultFromFetchA;
+          responseA.innerHTML = sRes;
+          var img = document.createElement("img"); 
+          img.src = "images/fox.jpg"; 
+          var src = document.getElementById("responseA"); 
+          src.appendChild(img);
+            
+          console.log("works")
+          return  getJSONFromServerUsingFetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`+resultFromFetchA.linkToNext)
+      }) .then(
+        function (resultFromFetchB) {
+          console.log(resultFromFetchB);
+          let sRes =  resultFromFetchB;
+          responseB.innerHTML = sRes;
+          return  getJSONFromServerUsingFetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`+resultFromFetchB.linkToNext)
+        })
+        .then(
+          function (resultFromFetchC) {
+            console.log(resultFromFetchC);
+            let sRes =  resultFromFetchC;
+            responseC.innerHTML = sRes;
+            return  getJSONFromServerUsingFetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`+resultFromFetchC.linkToNext)
+          })
+          .then(
+            function (resultFromFetchD) {
+              console.log(resultFromFetchD);
+              let sRes =  resultFromFetchD;
+              responseD.innerHTML = sRes;
+              return  getJSONFromServerUsingFetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`+resultFromFetchD.linkToNext)
+            })
+            .then(
+              function (resultFromFetchE) {
+                console.log(resultFromFetchE);
+                let sRes =  resultFromFetchE;
+                responseE.innerHTML = sRes;
+              })
+    } else if(userString === "paypal"){
+      answer = "You're close!! You just need to write it with its right spelling:)";
+      let almostThere = answer;
+      responseA.innerHTML = almostThere;
+
+    } else {
+      answer = "Wrong answer, try again";
+    let wrongAnswer = answer;
+    responseA.innerHTML = wrongAnswer;
+    }
+    console.log("time-out one-a complete " + answer);
+    return answer;
+  }, 2000); // let 5 secs go past then send back
 }
 
-//   buttonCallB.addEventListener('click', 
-  
-//   async function(){
-//     const results = document.getElementById("results");
-//     let  response = await fetch(`http://localhost:4200/sendSearch?${new URLSearchParams()}`);
-//     let responseText = await response.text();
-    
-//     // results.innerHTML = responseText;
-//     // Add a blank space between each line of the response text
-
-// console.log("the response::: ", responseText);
-
-// results.innerHTML = responseText;
-
-//   });//click
- 
-//   buttonCallC.addEventListener('click', 
-  
-//   async function(){
-//     const results = document.getElementById("results");
-//     let  response = await fetch(`http://localhost:4200/sendSearch?${new URLSearchParams('#search_hist')}`);
-//     let responseText = await response.text();
-    
-//     // results.innerHTML = responseText;
-//     // Add a blank space between each line of the response text
-
-// console.log("the response::: ", responseText);
-
-// results.innerHTML = responseText;
-
-//   });//click
- 
+function changeString_A(fruitString) {
+  //console.log(userString);
+  setTimeout(() => {
+    let userFruitwithstars = fruitString.split("").join("*");
+    console.log("time-out two-a complete " + fruitString);
+    return answer;
+  }, 2000); // let 5 secs go past then send back
 }
+
+ // format json object - helper :)
+// function formatJSObj(data) {
+//   return `
+//         ${data.title}
+//          <br />
+//          Description: ${data.description} <br /> `;
+// } 
+}
+
--- Assignment01/server.js
+++ Assignment01/server.js
@@ -21,9 +21,6 @@ async function run() {
   try {
 
     client.connect().then((res) => {
-      // A:
-    // await client.db("admin").command({ping:1});
-    // console.log("success");
     const db =  client.db("CART451_Final_Project");
     const videos =  db.collection("youtube_videos", {
       collation: { locale: "fr_CA",  numericOrdering: true,},});
@@ -34,122 +31,25 @@ async function run() {
         let r = await videos.createIndex({ category: 1 });
         let search = await videos.createIndex({ description: 1 });
         let s = await videos.createIndex({ title: 1 });
-        /* using the mongo $geonear:
-        we can filter results based on thre location coordinates ...*/
-        const buttonCallA = document.querySelector("#search_art");
-  const buttonCallB = document.querySelector('#search_food');
-  const buttonCallC = document.querySelector("#search_hist");
-        
-  if(buttonCallA.isClicked()){
           let responseArt = await videos.aggregate([
             {$match: {category:"art_music", description: {$lte:"400"}}},
               {$sort:{description:1}},
               {$project:{
                 _id:0,
-                link:0,
-              category:0,
+                link:1,
+                title:1,
+                description:1,
               },}
           ]).toArray();
-
-        }
-       else if(buttonCallB.isClicked){
-        let responseFood = await videos.aggregate([
-          {$match: {category:"food", description: {$lte:"400"}}},
-            {$sort:{description:1}},
-            {$project:{
-              _id:0,
-              link:0,
-            category:0,
-            },}
-        ]).toArray();
-       }else if(buttonCallC.isClicked()){
-        let responseHist = await videos.aggregate([
-          {$match: {category:"history", description: {$lte:"900"}}},
-            {$sort:{description:1}},
-            {$project:{
-              _id:0,
-              link:0,
-            category:0,
-            },}
-        ]).toArray();
-       }
       
-       res.send(responseFood,responseArt,responseHist);
+       res.send(responseArt);
           
-      //     // **6** Filter the art and music video category by displaying only the videos with less or equal than 400 subscribers on the channel, and only display their title and descriptions
-      // const pipeline = [ ]
-        
-      //   let filteredResults = await videos.aggregate(pipeline)
-  
-
-//     // **6** Filter the art and music video category by displaying only the videos with less or equal than 400 subscribers on the channel, and only display their title and descriptions
-// const pipeline = [ ]
-
-// //   let filteredResults = await videos.aggregate(pipeline)
-// for await (const doc of responseArt){
-//   console.log(doc);
-//   console.log("art achieved");
-// }
-
-// console.log(responseArt);
-
-    console.log("completed")
-
       };
 
       //3:: receiving serach criteria from the client
       app.use("/sendSearch", getSearchCrit);
       });
 
-//     // **1** Count the number of input in the collection
-//     const estimate = await videos.estimatedDocumentCount();
-//     console.log(`Estimated number of documents in the videos collection: ${estimate}`);
-    
-//     // **2** Display all the data in the console
-// let results = await videos.find({}).toArray();
-// console.log(results);
-
-
-// //**3** Retrieves the data from the history category and only display their title 
-// // Retrieves data
-//     const yt_video = "title";
-//     // Specify an optional query document to narrow results
-//     const content = { category: "history" };
-//     // Execute the distinct operation
-//     const distinctValues = await videos.distinct(yt_video, content);
-
-//     // Print the result
-//     console.log(distinctValues);
-
-    
-// // **4**Only find videos with subscribers over 100
-// const documentsToFind = {description:{$gt:"100"}}
-// let resultat = videos.find(documentsToFind);
-// let docCount = videos.countDocuments(documentsToFind);
-// await resultat.forEach((doc)=>console.log(doc))
-// console.log('Found ${await docCount} documents')
-
-// // **5** Find the video with the specific inserted id
-// const document = {_id: new ObjectId("65246667d654716acf8a431e")}
-// let answer = await videos.findOne(document)
-// console.log("Found document")
-// console.log(answer)
-
-
-// // **7** Find the food category videos, sort in alphabetical order the results, and limit the results to 5 videos
-// const neededDocuments = {category:"food"}
-// let foundResults = await videos.find(neededDocuments).sort({title:1}).limit(5);
-// await foundResults.forEach((doc)=>console.log(doc));
-// console.log('Found ${await docCounting} documents');
-
-
-// //**8**Find one video according to the inputed data categories
-// const options = {
-
-//   projection: {'description':1, 'category':1}
-// }
-// let isInGroup = await videos.findOne({'category':{$in:["beauty","vlogs", "travel", "food"]}}, options)
-// console.log(isInGroup);
 } // in try 
 catch (error) {
     console.error("error::");
